32624d73930d4c03fc6776c7c70d08cb554b77c1
Dan Sazonov <p.294803@gmail.com>
2022-02-18 14:29:41 +0300

    Wifi connection

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,10 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
 
 #define DS_PIN D3
+#define SSID "DSS WLAN S2"
+#define PASS "HoWXx4p1l0Rt"
 
 OneWire oneWire(DS_PIN);
 
@@ -10,8 +13,18 @@ DallasTemperature sensors(&oneWire);
 void setup() {
   Serial.begin(9600);
   sensors.begin();
-}
+  WiFi.begin(SSID, PASS);
 
+  Serial.print("Connecting to "); Serial.print(SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  } 
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println(WiFi.localIP());
+}
+  
 void loop() {
   sensors.requestTemperatures();
   float temperature_Celsius = sensors.getTempCByIndex(0);
